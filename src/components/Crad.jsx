1ee34b1ec8f8d884fb9aedde57e706a978427f44
Ray-han25 <abdulllahmutazab@gmail.com>
2026-09-05 21:56:01 +0600

    Your clear description of the new changes

--- src/components/Crad.jsx
+++ src/components/Crad.jsx
@@ -3,21 +3,24 @@ import Flex from './Flex'
 import { FaRegStar } from "react-icons/fa";
 import { CiHeart } from "react-icons/ci";
 import { FaEye } from "react-icons/fa";
-const Crad = ({ cradImg, cradText, price, disPrice, review ,}) => {
+const Crad = ({ cradImg, cradText, price, disPrice, review,Red_discoun, }) => {
     return (
         <>
-            <div>
-                <Flex className=' w-56.75 h-62.5 bg-[#f5f5f5]  items-center justify-center relative'>
-                    <img className=' align-middle' src={cradImg} alt="" />
-                    <div className=' absolute top-3 left-3 flex gap-31.25 '>
-                        <div className=' w-13.75 h-6.5 bg-p rounded-sm'></div>
-                        <div>
-                            <div className=' w-6 h-6 bg-white flex items-center justify-center text-black rounded-full'><CiHeart /></div>
-                            <div className=' w-6 h-6 bg-white flex items-center justify-center text-black rounded-full mt-2'> <FaEye/></div>
-                           
+            <div className='w-67.5 group'>
+                <div className=' relative w-fit overflow-hidden '>
+                    <Flex className=' w-56.75 h-62.5 bg-[#f5f5f5]  items-center justify-center relative'>
+                        <img className=' align-middle' src={cradImg} alt="" />
+                        <div className=' absolute top-3 left-3 flex gap-28.25 '>
+                            <div className=' py-1 pb-1 pl-3 pr-4 h-fit text-white bg-p rounded-sm'>-{Red_discoun}%</div>
+                            <div>
+                                <div className=' w-6 h-6 bg-white flex items-center justify-center text-black rounded-full'><CiHeart /></div>
+                                <div className=' w-6 h-6 bg-white flex items-center justify-center text-black rounded-full mt-2'> <FaEye /></div>
+
+                            </div>
                         </div>
-                    </div>
-                </Flex>
+                    </Flex>
+                    <button className=' w-full h-10.5 bg-black text-white absolute left-0 -bottom-11 group-hover:bottom-0  duration-300 rounded-sm'> Add To Cart</button>
+                </div>
                 <h2 className=' font-medium font-famailyP text-black mt-4 mb-2'>{cradText}</h2>
                 <div className=' flex gap-3 mb-2'>
                     <h2 className=' text-p'>${price}</h2>
--- src/components/Flash.jsx
+++ src/components/Flash.jsx
@@ -5,18 +5,32 @@ import Counter from './Counter'
 import Flex from './Flex'
 import Crad from './Crad'
 import consol from '../assets/consol.png'
+import __esModule from "react-slick";
 
 const Flash = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  }
+ console.log()
   return (
     <div className=' mt-41'>
       <Container>
         <Flex className={`gap-21.75 mb-10`}>
           <div><SectionHeder title="Today’s" Bigtext='Flash Sales' /></div>
-         <div className='mt-14.5'> <Counter /></div>
+          <div className='mt-14.5'> <Counter /></div>
         </Flex>
+          {/* <Slider {...settings}>
+
+
+
         <Crad
-        cradImg={consol} cradText='HAVIT HV-G92 Gamepad' price='120' disPrice='160' review='88'
+          cradImg={consol} cradText='HAVIT HV-G92 Gamepad' price='120' disPrice='160' review='88' Red_discoun='40'
         />
+          </Slider> */}
       </Container>
     </div>
   )
